06895dc4bc09cdeb4844aa5272654c160f9bae0c
JaiyeGesinde <jayegesinde@gmail.com>
2023-01-17 14:35:22 +0000

    robofriends-cards

---
+++ src/Robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Tolani Shaw',
+        username: 'TShaw',
+        email: 'tolanishaw@gmail.com',
+    },
+    {
+        id: 2,
+        name: 'Abi Obafemi',
+        username: 'AbbyO',
+        email: 'a.obafemi@gmail.com',
+    },
+    {
+        id: 3,
+        name: 'Deji Adebimpe',
+        username: 'Dj',
+        email: 'dejiadebimpe@gmail.com',
+    },
+    {
+        id: 4,
+        name: 'Nelson Ibekwe',
+        username: 'Neli',
+        email: 'nelsonibekwe@gmail.com',
+    },
+    {
+        id: 5,
+        name: 'Hamza Dauda',
+        username: 'Hamzy',
+        email: 'hamzadauda@gmail.com',
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import { robots } from './Robots';
+//import registerServiceWorker from './registerServiceWorker';
+//import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
+); */
+ReactDOM.render(
+                <div>
+                 <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email}/>
+                 <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
+                 <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
+                 <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email}/>
+                 <Card id={robots[4].id} name={robots[4].name} username={robots[4].username} email={robots[4].email}/>
+                </div>, 
+document.getElementById('root'));
+//registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
