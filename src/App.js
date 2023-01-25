64f4215bb0c4a9d50b4d6841d5d02ae029475cf4
JaiyeGesinde <jayegesinde@gmail.com>
2023-01-25 08:33:51 +0000

    fetch from fake API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, {Component} from "react";
-import { robots } from './Robots';
+//import { robots } from './Robots';
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import './App.css';
 
-
 class App extends Component {
-    constructor() {
-        super()
+        constructor() {
+            super()
             this.state = {
-                robots: robots,
+                robots: [],
                 seacrhfield: ''
             }
-    }
+        }
 
-    onSearchChange = (event) => {
-       this.setState({seacrhfield: event.target.value})
-    }
+        componentDidMount() {
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(Response=> Response.json())
+            .then(users=> this.setState({ robots: users}))
+        }
+
+        onSearchChange = (event) => {
+        this.setState({seacrhfield: event.target.value})
+        }
 
         render () {
             const filteredRobots = this.state.robots.filter(robots =>{
                 return robots.name.toLowerCase().includes(this.state.seacrhfield.toLowerCase())
             })
+            if (this.state.robots.length === 0) {
+                return <h1>loading</h1>
+            } else {
                 return (
                     <div className='tc'>
                         <h1 className="f1">Robofriends</h1>
@@ -29,6 +37,7 @@ class App extends Component {
                         <CardList robots = {filteredRobots}/>
                     </div>
                 );
+            }
         }
     
 }
